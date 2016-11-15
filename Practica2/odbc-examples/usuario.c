46df5839c38712fb7f9330b0c768fedf67ad6772
DavidGarciaFer <davidgar.fernandez@gmail.com>
2016-11-15 23:18:21 +0100

    P2.2 - usuario.c terminado, esta versión lee directamente de la línea de comandos, sin pedir nada por pantalla. Funciona perfectamente pero en caso de introducir los parámetros indebidos nos genera una violación de segmento. ATENCIÓN: Es posible que la versión de odbc.h tenga como nombre de la BD de datos library en vez de libreria, mea culpa, es que en mi ordenador lo tengo así.

--- Practica2/odbc-examples/usuario.c
+++ Practica2/odbc-examples/usuario.c
@@ -6,7 +6,7 @@
 #include "odbc.h"
 
 
-int main(void) {
+int main(int argc, char** argv) {
 
   SQLHENV env;
   SQLHDBC dbc;
@@ -17,10 +17,8 @@ int main(void) {
   SQLRETURN ret; /* ODBC API return status */
   SQLCHAR full_name[512];
   SQLCHAR screen_name[512];
-  char usuario[100];
   SQLINTEGER nousuario;
   SQLINTEGER y;
-  SQLCHAR sign;
 
   /* CONNECT */
   /*CONECTO CON LA BASE DE DATOS*/
@@ -40,94 +38,222 @@ int main(void) {
   SQLPrepare(stmt4, (SQLCHAR*) "select nousuario + 1 from usuario order by nousuario desc", SQL_NTS);
   /*Pedimos información al usuario*/
 
-  printf("Añadir o borrar usuario: \n");
-  fflush(stdout);
-  while (scanf("%s %c %s", usuario, &sign, screen_name) != EOF) {
+  if (argc < 3){
+    printf("Error en los parámetros de entrada:\n");
+    printf("./usuario + <screen_name> <full_name> (entre comillas) para insertar usuario.\n");
+    printf("./usuario - <screen_name> para borrar usuario.\n");
 
-      if(strcmp(usuario, "usuario") != 0){
-        printf("INPUT FAILURE - Syntax must be: \nusuario + <screen_name> <full_name> to create a new user\nusuario + <screen_name> to delete an user \n");
-        fflush(stdout);
-        printf("Añadir o borrar usuario: \n");
-        fflush(stdout);
-      }
+    /* free up statement handle */
+    SQLFreeHandle(SQL_HANDLE_STMT, stmt);
+    SQLFreeHandle(SQL_HANDLE_STMT, stmt2);
+    SQLFreeHandle(SQL_HANDLE_STMT, stmt3);
+    SQLFreeHandle(SQL_HANDLE_STMT, stmt4);
 
-      else if(sign == '+'){
-        scanf(" ");
-        /*Guardamos el nombre completo del usuario en una cadena*/
-        fgets((char*)full_name, 512, stdin);
+    /* DISCONNECT */
+    ret = odbc_disconnect(env, dbc);
+    if (!SQL_SUCCEEDED(ret)) {
+        return EXIT_FAILURE;
+    }
+  }
+
+  strcpy((char*)screen_name, argv[2]);
+
+    if(argv[1][0] == '+'){
+        if(argc < 4){
+          printf("Error en los parámetros de entrada:\n");
+          printf("./usuario + <screen_name> <full_name> (entre comillas) para insertar usuario.\n");
+
+          /* free up statement handle */
+          SQLFreeHandle(SQL_HANDLE_STMT, stmt);
+          SQLFreeHandle(SQL_HANDLE_STMT, stmt2);
+          SQLFreeHandle(SQL_HANDLE_STMT, stmt3);
+          SQLFreeHandle(SQL_HANDLE_STMT, stmt4);
+
+          /* DISCONNECT */
+          ret = odbc_disconnect(env, dbc);
+          if (!SQL_SUCCEEDED(ret)) {
+              return EXIT_FAILURE;
+          }
+        }
+
+        strcpy((char*)full_name, argv[3]);
+
         /*Añadimos el screen_name solicitado a nuestra query*/
         SQLBindParameter(stmt, 1, SQL_PARAM_INPUT, SQL_C_CHAR, SQL_CHAR, 0, 0, screen_name, 0, NULL);
         /*Ejecutamos la query*/
-        SQLExecute(stmt);
-        /*Asignamos el valor de a primera columna a la variable nousuario*/
+        if (!SQL_SUCCEEDED(ret = SQLExecute(stmt))) {
+            printf("Error al comprobar si existe el usuario solicitado.\n");
+            fflush(stdout);
+            SQLCloseCursor(stmt);
+            /* free up statement handle */
+            SQLFreeHandle(SQL_HANDLE_STMT, stmt);
+            SQLFreeHandle(SQL_HANDLE_STMT, stmt2);
+            SQLFreeHandle(SQL_HANDLE_STMT, stmt3);
+            SQLFreeHandle(SQL_HANDLE_STMT, stmt4);
+
+            /* DISCONNECT */
+            ret = odbc_disconnect(env, dbc);
+            if (!SQL_SUCCEEDED(ret)) {
+                return EXIT_FAILURE;
+            }
+        }
+        /*Asignamos el valor de la primera columna a la variable nousuario*/
         SQLBindCol(stmt, 0, SQL_C_SBIGINT, &nousuario, sizeof(nousuario), NULL);
+
         if (SQL_SUCCEEDED(ret = SQLFetch(stmt))) {
-            printf("%d", ret); /*Aquí por alguna razon ret es 0*/
             printf("El usuario %s ya existe\n", screen_name);
             SQLCloseCursor(stmt);
-            printf("Añadir o borrar usuario: \n");
-            fflush(stdout);
-        }
+            /* free up statement handle */
+            SQLFreeHandle(SQL_HANDLE_STMT, stmt);
+            SQLFreeHandle(SQL_HANDLE_STMT, stmt2);
+            SQLFreeHandle(SQL_HANDLE_STMT, stmt3);
+            SQLFreeHandle(SQL_HANDLE_STMT, stmt4);
 
+            /* DISCONNECT */
+            ret = odbc_disconnect(env, dbc);
+            if (!SQL_SUCCEEDED(ret)) {
+                return EXIT_FAILURE;
+            }
+        }
         else{
-          SQLExecute(stmt4);
-          SQLBindCol(stmt4, 1, SQL_C_SLONG, &y, sizeof(y), NULL);
-          if (!SQL_SUCCEEDED(ret = SQLFetch(stmt4))) {
-              printf("ERROR EN EL FETCH - NO SE PUDO OBTENER NUEVO ID DE USUARIO\n");
+          if (!SQL_SUCCEEDED(ret = SQLExecute(stmt4))) {
+              printf("Error 1 al insertar usuario.\n");
               fflush(stdout);
               SQLCloseCursor(stmt4);
+              SQLCloseCursor(stmt);
+              /* free up statement handle */
+              SQLFreeHandle(SQL_HANDLE_STMT, stmt);
+              SQLFreeHandle(SQL_HANDLE_STMT, stmt2);
+              SQLFreeHandle(SQL_HANDLE_STMT, stmt3);
+              SQLFreeHandle(SQL_HANDLE_STMT, stmt4);
+
+              /* DISCONNECT */
+              ret = odbc_disconnect(env, dbc);
+              if (!SQL_SUCCEEDED(ret)) {
+                  return EXIT_FAILURE;
+              }
+          }
+          SQLBindCol(stmt4, 1, SQL_C_SLONG, &y, sizeof(y), NULL);
+          if (!SQL_SUCCEEDED(ret = SQLFetch(stmt4))) {
+              printf("Error 2 al insertar usuario.\n");
+              fflush(stdout);
+              SQLCloseCursor(stmt4);
+              SQLCloseCursor(stmt);
+              /* free up statement handle */
+              SQLFreeHandle(SQL_HANDLE_STMT, stmt);
+              SQLFreeHandle(SQL_HANDLE_STMT, stmt2);
+              SQLFreeHandle(SQL_HANDLE_STMT, stmt3);
+              SQLFreeHandle(SQL_HANDLE_STMT, stmt4);
+
+              /* DISCONNECT */
+              ret = odbc_disconnect(env, dbc);
+              if (!SQL_SUCCEEDED(ret)) {
+                  return EXIT_FAILURE;
+              }
           }
           else{
             SQLBindParameter(stmt2, 1, SQL_PARAM_INPUT, SQL_C_SLONG, SQL_INTEGER, 0, 0, &y, 0, NULL);
             SQLBindParameter(stmt2, 2, SQL_PARAM_INPUT, SQL_C_CHAR, SQL_CHAR, 0, 0, screen_name, 0, NULL);
             SQLBindParameter(stmt2, 3, SQL_PARAM_INPUT, SQL_C_CHAR, SQL_CHAR, 0, 0, full_name, 0, NULL);
-            SQLExecute(stmt2);
+            if (!SQL_SUCCEEDED(ret = SQLExecute(stmt2))) {
+                printf("Error 3 al insertar usuario.\n");
+                fflush(stdout);
+                SQLCloseCursor(stmt4);
+                SQLCloseCursor(stmt);
+                SQLCloseCursor(stmt2);
+                /* free up statement handle */
+                SQLFreeHandle(SQL_HANDLE_STMT, stmt);
+                SQLFreeHandle(SQL_HANDLE_STMT, stmt2);
+                SQLFreeHandle(SQL_HANDLE_STMT, stmt3);
+                SQLFreeHandle(SQL_HANDLE_STMT, stmt4);
+
+                /* DISCONNECT */
+                ret = odbc_disconnect(env, dbc);
+                if (!SQL_SUCCEEDED(ret)) {
+                    return EXIT_FAILURE;
+                }
+            }
             printf("Usuario añadido con éxito.\n");
             fflush(stdout);
             SQLCloseCursor(stmt);
             SQLCloseCursor(stmt4);
             SQLCloseCursor(stmt2);
-            printf("Añadir o borrar usuario: \n");
-            fflush(stdout);
-        }
+          }
         }
       }
 
-      else if(sign == '-'){
-        fflush(stdout);
+      else if(argv[1][0] == '-'){
         /*Añadimos el screen_name solicitado a nuestra query*/
         SQLBindParameter(stmt, 1, SQL_PARAM_INPUT, SQL_C_CHAR, SQL_CHAR, 0, 0, screen_name, 0, NULL);
         /*Ejecutamos la query*/
-        SQLExecute(stmt);
+        if (!SQL_SUCCEEDED(ret = SQLExecute(stmt))) {
+            printf("Error al comprobar si existe el usuario solicitado.\n");
+            fflush(stdout);
+            SQLCloseCursor(stmt);
+            /* free up statement handle */
+            SQLFreeHandle(SQL_HANDLE_STMT, stmt);
+            SQLFreeHandle(SQL_HANDLE_STMT, stmt2);
+            SQLFreeHandle(SQL_HANDLE_STMT, stmt3);
+            SQLFreeHandle(SQL_HANDLE_STMT, stmt4);
+
+            /* DISCONNECT */
+            ret = odbc_disconnect(env, dbc);
+            if (!SQL_SUCCEEDED(ret)) {
+                return EXIT_FAILURE;
+            }
+        }
         /*Asignamos el valor de a primera columna a la variable nousuario*/
         SQLBindCol(stmt, 1, SQL_INTEGER, &nousuario, sizeof(nousuario), NULL);
         if (SQL_SUCCEEDED(ret = SQLFetch(stmt))) {
             SQLBindParameter(stmt3, 1, SQL_PARAM_INPUT, SQL_C_CHAR, SQL_CHAR, 0, 0, screen_name, 0, NULL);
-            SQLExecute(stmt3);
+            if (!SQL_SUCCEEDED(ret = SQLExecute(stmt3))) {
+                printf("Error al borrar el usuario solicitado.\n");
+                fflush(stdout);
+                SQLCloseCursor(stmt);
+                SQLCloseCursor(stmt);
+                /* free up statement handle */
+                SQLFreeHandle(SQL_HANDLE_STMT, stmt);
+                SQLFreeHandle(SQL_HANDLE_STMT, stmt2);
+                SQLFreeHandle(SQL_HANDLE_STMT, stmt3);
+                SQLFreeHandle(SQL_HANDLE_STMT, stmt4);
+
+                /* DISCONNECT */
+                ret = odbc_disconnect(env, dbc);
+                if (!SQL_SUCCEEDED(ret)) {
+                    return EXIT_FAILURE;
+                }
+            }
             SQLCloseCursor(stmt);
             SQLCloseCursor(stmt3);
             printf("Usuario %s borrado con éxito\n", screen_name);
-            fflush(stdout);
-            printf("Añadir o borrar usuario: \n");
             fflush(stdout);
         }
         else{
           SQLCloseCursor(stmt);
           printf("El usuario introducido no se encuentra en la base de datos.\n");
           fflush(stdout);
-          printf("Añadir o borrar usuario: \n");
-          fflush(stdout);
         }
       }
 
       else {
-        printf("INPUT FAILURE - Incorrect instruction syntax:\n<usuario +> to add a new user\n<usuario -> to delete an user\n");
+        printf("Error en los parámetros de entrada:\n");
+        printf("./usuario + <screen_name> <full_name> (entre comillas) para insertar usuario.\n");
+        printf("./usuario - <screen_name> para borrar usuario.\n");
         fflush(stdout);
-        printf("Añadir o borrar usuario: \n");
-        fflush(stdout);
+        /* free up statement handle */
+        SQLFreeHandle(SQL_HANDLE_STMT, stmt);
+        SQLFreeHandle(SQL_HANDLE_STMT, stmt2);
+        SQLFreeHandle(SQL_HANDLE_STMT, stmt3);
+        SQLFreeHandle(SQL_HANDLE_STMT, stmt4);
+
+        /* DISCONNECT */
+        ret = odbc_disconnect(env, dbc);
+        if (!SQL_SUCCEEDED(ret)) {
+            return EXIT_FAILURE;
+        }
+
       }
 
-  }
 
   /* free up statement handle */
   SQLFreeHandle(SQL_HANDLE_STMT, stmt);
