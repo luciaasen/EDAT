18fc025b653227ddc033c1844f5615af8b63c678
DavidGarciaFer <davidgar.fernandez@gmail.com>
2016-11-30 19:50:50 +0100

    Comenzamos con la implementacion de table.c

--- Practica3/esecuele/development/database/record.c
+++ Practica3/esecuele/development/database/record.c
@@ -2,14 +2,30 @@
 
 struct record_ {
   /* To be implemented */
+  /*
+    void ** values;
+    int ncols
+    long next
+
+  */
 };
 
 record_t* record_create(void** values, int ncols, long next) {
   /* To be implemented */
+  /*
+    malloc record
+    values
+    ncols
+    next
+    (igual que en table)
+  */
 }
 
 void* record_get(record_t* record, int n) {
   /* To be implemented */
+  /*
+    devolver record->values[n]
+  */
 }
 
 long record_next(record_t* record) {
@@ -18,4 +34,6 @@ long record_next(record_t* record) {
 
 void record_free(record_t* record) {
   /* To be implemented */
+  /*Libera toda la memoria*/
+  /*record values y de record*/
 }
--- Practica3/esecuele/development/database/table.c
+++ Practica3/esecuele/development/database/table.c
@@ -6,14 +6,57 @@
 
 struct table_ {
   /* To be implemented */
+  FILE * fichero;
+  int ncols;
+  long first_pos;
+  long last_pos;
 };
 
 void table_create(char* path, int ncols, type_t* types) {
-  /* To be implemented */
+  /*
+    fopen(path, "w")
+    meter la cabecera con fwrite del ncols y de los tipos
+    ej: fwrite(types, sizeof(type_t), n_cols, nobrefichero)
+  */
+  FILE* file;
+  file = fopen(path, "w");
+  if(!file)
+    return;
+  fwrite(n_cols, sizeof(int), 1, file);
+  fwrite(types, sizeof(type_t), n_cols, file);
+  fclose(file);
 }
 
 table_t* table_open(char* path) {
-  /* To be implemented */
+  /*
+    Hacemos un malloc de table
+    table->file = fopen
+    leemos ncols table->ncols = fread
+    leemos los tipos
+      ¿¿¿¿¿¿ malloc de table->types ?????
+      fread table->types
+    table->first_pos = ftell
+    table->last_pos = "fin archivo"
+  */
+  table_t* table;
+
+  /*Creamos la tabla*/
+
+  table = (table_t*)malloc(sizeof(table_t));
+  if(!table)
+    return NULL;
+
+  table->fichero = fopen(path, "r");
+  if(!table->fichero)
+    return NULL;
+
+  /*Obtenemos el número de columnas de la tabla*/
+  fread(&(table->ncols), sizeof(int), 1, table->fichero);
+
+  fseek(table->fichero, table->ncols*sizeof(type_t), SEEK_CUR);
+  table_first_pos = ftell(table->fichero);
+  /*FALTA EL FINAL Y DEVOLVER*/
+
 }
 
 void table_close(table_t* table) {
@@ -38,8 +81,20 @@ long table_last_pos(table_t* table) {
 
 record_t* table_read_record(table_t* table, long pos) {
   /* To be implemented */
+  /*void** values*/
+  /*Comprobar que la posicion que nos pasan no typeses la ultima (EOF)*/
+  /*Hacemos fseek*/
+  /*malloc de values = malloc(table->ncols*sizeof(void*))*/
+  /*for(i, i ?< ncols, i++){
+    fread(&size, sizeof(type_t), 1, table->file)
+    values = malloc(size);
+    fread(values[i], size, 1, tabla->file)
+    next = ftell
+    record_create(values, table->ncols, next);
+  }*/
 }
 
 void table_insert_record(table_t* table, void** values) {
   /* To be implemented */
+  /*Pensar nosotros*/
 }
