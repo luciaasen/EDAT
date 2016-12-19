033ca2c163e7249a43c8164674826b26543c575f
DavidGarciaFer <davidgar.fernandez@gmail.com>
2016-12-19 21:38:52 +0100

    Acabo mi parte de la memoria

--- Practica3/esecuele/development/libreria.bash
+++ Practica3/esecuele/development/libreria.bash
@@ -1,44 +1,38 @@
 COMMAND="./esecuele"
 
-rm -rf libreria_db
+rm -rf libros_db
 
-echo "Creando libreria"
-$COMMAND createdb libreria_db
+echo "Creando libros_db"
+$COMMAND createdb libros_db
 
 echo "Creando tablas"
-$COMMAND define libreria_db << EOF
+$COMMAND define libros_db << EOF
 TABLE edicion 7 STR STR STR STR STR STR STR
 TABLE compra 5 STR LNG STR STR INT
 TABLE usuario 5 LNG STR STR STR STR STR
 EOF
 
 echo "Insertando datos en tablas"
-$COMMAND insert libreria_db << EOF
+$COMMAND insert libros_db << EOF
 COPY edicion ./example_files/edicion.txt
 COPY compra ./example_files/compra.txt
 COPY usuario ./example_files/usuario.txt
 EOF
 
 echo "Título de los libros comprados por jack hecho por David:"
-$COMMAND query libreria_db << EOF
+$COMMAND query libros_db << EOF
 usuario SEQUENTIAL 2 STR jack C_COLEQCTE SELECT compra SEQUENTIAL PRODUCT 0 6 C_COLEQCOL SELECT STR 5 P_COL 1 PROJECT edicion SEQUENTIAL PRODUCT 0 1 C_COLEQCOL SELECT STR 6 P_COL 1 PROJECT
 EOF
 echo  "Número de libros comprados por jack hecho por David:"
-$COMMAND query libreria_db << EOF
+$COMMAND query libros_db << EOF
 usuario SEQUENTIAL 2 STR jack C_COLEQCTE SELECT compra SEQUENTIAL PRODUCT 0 6 C_COLEQCOL SELECT COUNT
 EOF
 
 echo "Título de los libros comprados por jack hecho por Lucia:"
-$COMMAND query libreria_db << EOF
+$COMMAND query libros_db << EOF
 compra SEQUENTIAL usuario SEQUENTIAL 2 STR jack C_COLEQCTE SELECT LNG 0 P_COL 1 PROJECT PRODUCT 1 5 C_COLEQCOL SELECT STR 0 P_COL 1 PROJECT edicion SEQUENTIAL STR 0 P_COL STR 5 P_COL 2 PROJECT PRODUCT 0 1 C_COLEQCOL SELECT STR 2 P_COL 1 PROJECT
 EOF
 echo  "Número de libros comprados por jack hecho por Lucía:"
-$COMMAND query libreria_db << EOF
-compra SEQUENTIAL usuario SEQUENTIAL 2 STR jack C_COLEQCTE SELECT LNG 0 P_COL 1 PROJECT PRODUCT 1 5 C_COLEQCOL SELECT COUNT 
+$COMMAND query libros_db << EOF
+compra SEQUENTIAL usuario SEQUENTIAL 2 STR jack C_COLEQCTE SELECT LNG 0 P_COL 1 PROJECT PRODUCT 1 5 C_COLEQCOL SELECT COUNT
 EOF
-
-
-
-
-
-
