e8b58dc87b610f3fd487aaf802cad9a9f3f43efd
luciaasen <luciaasen@gmail.com>
2016-12-19 23:32:02 +0100

    memoria terminada?

--- Practica3/esecuele/development/libreria.bash
+++ Practica3/esecuele/development/libreria.bash
@@ -19,20 +19,20 @@ COPY compra ./example_files/compra.txt
 COPY usuario ./example_files/usuario.txt
 EOF
 
-echo "Título de los libros comprados por jack hecho por David:"
-$COMMAND query libros_db << EOF
-usuario SEQUENTIAL 2 STR jack C_COLEQCTE SELECT compra SEQUENTIAL PRODUCT 0 6 C_COLEQCOL SELECT STR 5 P_COL 1 PROJECT edicion SEQUENTIAL PRODUCT 0 1 C_COLEQCOL SELECT STR 6 P_COL 1 PROJECT
-EOF
-echo  "Número de libros comprados por jack hecho por David:"
-$COMMAND query libros_db << EOF
-usuario SEQUENTIAL 2 STR jack C_COLEQCTE SELECT compra SEQUENTIAL PRODUCT 0 6 C_COLEQCOL SELECT COUNT
-EOF
+## echo "Título de los libros comprados por jack hecho por David:"
+## $COMMAND query libros_db << EOF
+## usuario SEQUENTIAL 2 STR jack C_COLEQCTE SELECT compra SEQUENTIAL PRODUCT 0 6 C_COLEQCOL SELECT STR 5 P_COL 1 PROJECT edicion SEQUENTIAL PRODUCT 0 1 C_COLEQCOL SELECT STR 6 P_COL 1 PROJECT
+## EOF
+## echo  "Número de libros comprados por jack hecho por David:"
+## $COMMAND query libros_db << EOF
+## usuario SEQUENTIAL 2 STR jack C_COLEQCTE SELECT compra SEQUENTIAL PRODUCT 0 6 C_COLEQCOL SELECT COUNT
+## EOF
 
-echo "Título de los libros comprados por jack hecho por Lucia:"
+echo "Título de los libros comprados por jack:"
 $COMMAND query libros_db << EOF
 compra SEQUENTIAL usuario SEQUENTIAL 2 STR jack C_COLEQCTE SELECT LNG 0 P_COL 1 PROJECT PRODUCT 1 5 C_COLEQCOL SELECT STR 0 P_COL 1 PROJECT edicion SEQUENTIAL STR 0 P_COL STR 5 P_COL 2 PROJECT PRODUCT 0 1 C_COLEQCOL SELECT STR 2 P_COL 1 PROJECT
 EOF
-echo  "Número de libros comprados por jack hecho por Lucía:"
+echo  "Número de libros comprados por jack:"
 $COMMAND query libros_db << EOF
 compra SEQUENTIAL usuario SEQUENTIAL 2 STR jack C_COLEQCTE SELECT LNG 0 P_COL 1 PROJECT PRODUCT 1 5 C_COLEQCOL SELECT COUNT
 EOF
